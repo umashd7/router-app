4389e6ce908b85e261c4eebce35818c4599367e5
Umash Tatavarthy <umashd7@gmail.com>
2024-07-13 11:34:11 +0530

    Adds env var in homepage

--- src/routes/Homepage.js
+++ src/routes/Homepage.js
@@ -3,8 +3,8 @@ import { Outlet } from "react-router-dom";
 function HomePage() {
   return (
     <>
-      <div className="App-header">Welcome to Home page</div>
-      <div> { `You are in ${process.env.REACT_APP_ENV} environment`} </div>
+      <div className="App-header">Welcome to {process.env.REACT_APP_ENV} Home page
+      </div>
       <Outlet/>
     </>
   );
